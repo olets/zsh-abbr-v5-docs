b22ca34343971f9d09585ac97faf63c541e6b308
Henry Bley-Vroman <olets@olets.dev>
2022-01-01 12:13:31 -1000

    style(config): format with eslint

--- docs/.vuepress/configs/navbar.ts
+++ docs/.vuepress/configs/navbar.ts
@@ -1,26 +1,26 @@
 // See https://github.com/vuepress/vuepress-next/blob/main/docs/.vuepress/configs/navbar/en.ts
 
-import type { NavbarConfig } from '@vuepress/theme-default'
+import type { NavbarConfig } from "@vuepress/theme-default";
 
 export const navbar: NavbarConfig = [
   {
-    text: 'Guide',
-    link: '/guide/',
+    text: "Guide",
+    link: "/guide/",
   },
   {
-    text: 'Changelog',
-    link: 'https://github.com/olets/zsh-abbr/blob/main/CHANGELOG.md',
+    text: "Changelog",
+    link: "https://github.com/olets/zsh-abbr/blob/main/CHANGELOG.md",
   },
   {
-    text: 'Roadmap',
-    link: 'https://github.com/olets/zsh-abbr/blob/main/ROADMAP.md',
+    text: "Roadmap",
+    link: "https://github.com/olets/zsh-abbr/blob/main/ROADMAP.md",
   },
   {
-    text: 'Contributing',
-    link: 'https://github.com/olets/zsh-abbr/blob/main/CONTRIBUTING.md',
+    text: "Contributing",
+    link: "https://github.com/olets/zsh-abbr/blob/main/CONTRIBUTING.md",
   },
   {
-    text: 'License',
-    link: 'https://github.com/olets/zsh-abbr/blob/main/LICENSE',
+    text: "License",
+    link: "https://github.com/olets/zsh-abbr/blob/main/LICENSE",
   },
-]
+];
--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -1,23 +1,23 @@
 // https://github.com/vuepress/vuepress-next/blob/main/docs/.vuepress/configs/sidebar/en.ts
 // https://vuepress.github.io/reference/default-theme/config.html#sidebar
 
-import type { SidebarConfig } from '@vuepress/theme-default'
+import type { SidebarConfig } from "@vuepress/theme-default";
 
 export const sidebar: SidebarConfig = {
-  '/': [
+  "/": [
     {
-      text: 'Guide',
+      text: "Guide",
       children: [
         {
-          link: '/README.md',
-          text: 'Introduction',
+          link: "/README.md",
+          text: "Introduction",
         },
-        '/installation',
-        '/usage.md',
-        '/advanced.md',
-        '/performance.md',
-        '/uninstalling.md',
-      ]
-    }
-  ]
-}
+        "/installation",
+        "/usage.md",
+        "/advanced.md",
+        "/performance.md",
+        "/uninstalling.md",
+      ],
+    },
+  ],
+};
